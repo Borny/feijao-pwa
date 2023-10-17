b43c8b4add3e3d4cdc5b434781a5dc76861c111d
Tristan <borntowebb@gmail.com>
2023-10-17 22:22:12 +0200

    updated the api url

---
+++ assets/use-quasar.5cfbc034.js
@@ -0,0 +1,1 @@
+import{i as a,aK as r}from"./index.c897d222.js";function u(){return a(r)}export{u};
--- assets/vue-i18n.192b1502.js
+++ assets/vue-i18n.192b1502.js
@@ -1,4 +1,4 @@
-import{d as ft,h as Qt,g as je,e as On,i as kn,o as vn,a as yn,s as Cn,r as ne,c as ae,b as An,w as Ze,F as qt,f as Pn,j as Rn,T as Sn}from"./index.3e321f06.js";/*!
+import{d as ft,h as Qt,g as je,e as On,i as kn,o as vn,a as yn,s as Cn,r as ne,c as ae,b as An,w as Ze,F as qt,f as Pn,j as Rn,T as Sn}from"./index.c897d222.js";/*!
   * shared v9.4.1
   * (c) 2023 kazuya kawaguchi
   * Released under the MIT License.
