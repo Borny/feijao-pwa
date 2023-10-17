4f0e0718c32bc2fa8d55e80f0d7f08e1fa96f811
Tristan <borntowebb@gmail.com>
2023-10-17 15:42:40 +0200

    Updated the vapid key

--- dist/pwa/assets/app.5b3c8861.js
+++
@@ -1,1 +0,0 @@
-import{aQ as t}from"./index.2d0008b8.js";var a={};const p=t("app",{state:()=>({appTitle:null}),actions:a});export{p as u};
---
+++ dist/pwa/assets/use-quasar.7b1fc602.js
@@ -0,0 +1,1 @@
+import{i as a,aK as r}from"./index.3e321f06.js";function u(){return a(r)}export{u};
