5a93c0aadd141748af2f70bbdb2484380bc82e06
Tristan <borntowebb@gmail.com>
2023-10-17 13:53:24 +0200

    Added push notifications

---
+++ assets/app.5b3c8861.js
@@ -0,0 +1,1 @@
+import{aQ as t}from"./index.2d0008b8.js";var a={};const p=t("app",{state:()=>({appTitle:null}),actions:a});export{p as u};
---
+++ assets/use-quasar.43b71169.js
@@ -0,0 +1,1 @@
+import{i as a,aK as r}from"./index.2d0008b8.js";function u(){return a(r)}export{u};
