8fd0a7a8137f1f8b96a846e201b23ee8f6ba09e9
Tristan <borntowebb@gmail.com>
2023-10-18 10:39:21 +0200

    Updated the prod api url to aws

---
+++ assets/app.4d423b5b.js
@@ -0,0 +1,1 @@
+import{aQ as t}from"./index.e125f141.js";var a={};const p=t("app",{state:()=>({appTitle:null}),actions:a});export{p as u};
---
+++ assets/use-quasar.9041e38c.js
@@ -0,0 +1,1 @@
+import{i as a,aK as r}from"./index.e125f141.js";function u(){return a(r)}export{u};
