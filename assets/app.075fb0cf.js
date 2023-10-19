348c6567ba6eef4585eb4c8e8aa6556c44b277d9
Tristan <borntowebb@gmail.com>
2023-10-19 20:25:20 +0200

    Fixed the push notifications

---
+++ assets/app.075fb0cf.js
@@ -0,0 +1,1 @@
+import{aQ as t}from"./index.49049b66.js";var a={};const p=t("app",{state:()=>({appTitle:null}),actions:a});export{p as u};
--- assets/app.d4a6d0a1.js
+++
@@ -1,1 +0,0 @@
-import{aQ as t}from"./index.cd28cc48.js";var a={};const p=t("app",{state:()=>({appTitle:null}),actions:a});export{p as u};
