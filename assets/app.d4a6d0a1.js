f5b9e43aefb14232fe55e69df3c7be6733e1b038
Tristan <borntowebb@gmail.com>
2023-10-19 15:26:10 +0200

    Updated the API URL to Railway

---
+++ assets/app.d4a6d0a1.js
@@ -0,0 +1,1 @@
+import{aQ as t}from"./index.cd28cc48.js";var a={};const p=t("app",{state:()=>({appTitle:null}),actions:a});export{p as u};
--- assets/use-quasar.9041e38c.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aK as r}from"./index.e125f141.js";function u(){return a(r)}export{u};
