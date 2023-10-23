0069e274c021a2a8d8002b63d31e5610c5f93988
Tristan <borntowebb@gmail.com>
2023-10-23 22:24:23 +0200

    Added the nickname to the comments

---
+++ assets/use-quasar.5958bcb7.js
@@ -0,0 +1,1 @@
+import{i as a,aK as r}from"./index.5863b37e.js";function u(){return a(r)}export{u};
--- assets/use-quasar.ca13689e.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aK as r}from"./index.e0833c94.js";function u(){return a(r)}export{u};
