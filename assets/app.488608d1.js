b6125e3811e5baddfbc5708ceba24c1af0b9ee1d
Tristan <borntowebb@gmail.com>
2023-11-01 22:16:41 +0100

    Commented the like button

--- assets/app.488608d1.js
+++
@@ -1,1 +0,0 @@
-import{aQ as t}from"./index.ace5c7f6.js";var a={};const p=t("app",{state:()=>({appTitle:null}),actions:a});export{p as u};
---
+++ assets/use-quasar.976624be.js
@@ -0,0 +1,1 @@
+import{i as a,aK as r}from"./index.4a090d90.js";function u(){return a(r)}export{u};
